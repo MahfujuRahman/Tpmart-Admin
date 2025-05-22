267fefd85d1e06cf1563896c3d630fc64ed1c4ea
MahfujuRahman <akashajmain0@gmail.com>
2025-05-22 16:20:17 +0600

    Enhance sidebar by adding product counts to categories and improving code formatting

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -83,10 +83,21 @@
     </li>
 
     <li>
-        <a href="{{ url('/view/all/category') }}"><i class="feather-sliders"></i><span>Category</span></a>
+        <a href="{{ url('/view/all/category') }}">
+            <i class="feather-sliders"></i>
+            <span>Category</span>
+            <span style="color:lightgreen" title="Total Products">
+                ({{DB::table('categories')->count()}})
+            </span>
+        </a>
     </li>
     <li>
-        <a href="{{ url('/view/all/subcategory') }}"><i class="feather-command"></i><span>Subcategory</span></a>
+        <a href="{{ url('/view/all/subcategory') }}"><i class="feather-command"></i>
+            <span>Subcategory</span>
+            <span style="color:lightgreen" title="Total Products">
+                ({{DB::table('subcategories')->count()}})
+            </span>
+        </a>
         {{-- <ul class="sub-menu" aria-expanded="false">
             <li><a href="{{ url('/add/new/subcategory') }}">Add New Subcategory</a></li>
             <li><a href="{{ url('/view/all/subcategory') }}">View All Subcategories</a></li>
@@ -94,7 +105,12 @@
     </li>
     <li>
         <a href="{{ url('/view/all/childcategory') }}"><i class="feather-git-pull-request"></i><span>Child
-                Category</span></a>
+                Category</span>
+            <span style="color:lightgreen" title="Total Products">
+                ({{DB::table('child_categories')->count()}})
+            </span>
+
+        </a>
     </li>
     <li>
         <a href="javascript: void(0);" class="has-arrow"><i class="feather-box"></i><span>Manage Products</span></a>
@@ -231,7 +247,12 @@
         </ul>
     </li> --}}
     <li>
-        <a href="{{ url('/view/all/promo/codes') }}"><i class="feather-gift"></i><span>Promo Codes</span></a>
+        <a href="{{ url('/view/all/promo/codes') }}"><i class="feather-gift"></i>
+            <span>Promo Codes</span>
+            <span style="color:lightgreen" title="Total Products">
+                ({{DB::table('promo_codes')->count()}})
+            </span>
+        </a>
     </li>
 
     {{-- <li><a href="{{ url('/file-manager') }}"><i class="fas fa-folder-open"></i><span>File Manager</span></a></li>
@@ -593,54 +614,54 @@
 
     <li>
         <a href="{{ url('/download/database/backup') }}"
-           onclick="return confirm('Are you sure you want to download the database backup?');">
+            onclick="return confirm('Are you sure you want to download the database backup?');">
             <i class="feather-database"></i>
             Database Backup
         </a>
     </li>
     <li>
         <a href="{{ url('/download/product/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the product images backup?');">
+            onclick="return confirm('Are you sure you want to download the product images backup?');">
             <i class="feather-image"></i>Product Images Backup</a>
     </li>
     <li>
         <a href="{{ url('/download/user/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the user images backup?');">
+            onclick="return confirm('Are you sure you want to download the user images backup?');">
             <i class="feather-user"></i>User Images Backup</a>
     </li>
     <li>
         <a href="{{ url('/download/banner/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the banner images backup?');">
+            onclick="return confirm('Are you sure you want to download the banner images backup?');">
             <i class="feather-layers"></i>Banner Images Backup</a>
     </li>
     <li>
         <a href="{{ url('/download/category/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the category icon backup?');">
+            onclick="return confirm('Are you sure you want to download the category icon backup?');">
             <i class="feather-grid"></i>Category Icon Backup</a>
     </li>
     <li>
         <a href="{{ url('/download/subcategory/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the subcategory backup?');">
+            onclick="return confirm('Are you sure you want to download the subcategory backup?');">
             <i class="feather-list"></i>Subcategory Backup</a>
     </li>
     <li>
         <a href="{{ url('/download/flag/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the flag icon backup?');">
+            onclick="return confirm('Are you sure you want to download the flag icon backup?');">
             <i class="feather-flag"></i>Flag Icon Backup</a>
     </li>
     <li>
         <a href="{{ url('/download/ticket/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the ticket files backup?');">
+            onclick="return confirm('Are you sure you want to download the ticket files backup?');">
             <i class="feather-file"></i>Ticket Files Backup</a>
     </li>
     <li>
         <a href="{{ url('/download/blog/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the blog files backup?');">
+            onclick="return confirm('Are you sure you want to download the blog files backup?');">
             <i class="feather-file-text"></i>Blog Files Backup</a>
     </li>
     <li>
         <a href="{{ url('/download/other/files/backup') }}"
-           onclick="return confirm('Are you sure you want to download the other images backup?');">
+            onclick="return confirm('Are you sure you want to download the other images backup?');">
             <i class="feather-folder"></i>Other Images Backup</a>
     </li>
 
@@ -654,8 +675,11 @@
         </ul>
     </li>
     <li><a href="{{ url('/clear/cache') }}"><i class="feather-rotate-cw"></i><span>Clear Cache</span></a></li>
-    <li><a href="{{ route('logout') }}"
-            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
-                class="feather-log-out"></i><span>Logout</span></a></li>
+    <li>
+        <a href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <i class="feather-log-out"></i><span>Logout</span>
+        </a>
+    </li>
 
 </ul>
